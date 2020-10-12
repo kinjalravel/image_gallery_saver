1f57c9481a8ccf7a3510af03f7cb07adf73ceeb5
kinjalravel <kinjalraval18591@gmail.com>
2020-10-12 19:01:36 +0530

    Update ImageGallerySaverPlugin.kt

--- android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -1,5 +1,6 @@
 package com.example.imagegallerysaver
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
@@ -7,24 +8,35 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
-class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandler {
 
+class ImageGallerySaverPlugin: MethodCallHandler,FlutterPlugin {
+    private var context:Context? = null;
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "image_gallery_saver")
-      channel.setMethodCallHandler(ImageGallerySaverPlugin(registrar))
+      val plugin = ImageGallerySaverPlugin()
+      plugin.context = registrar.context()
+      register(plugin,registrar.messenger())
+    }
+
+    private fun register(plugin: ImageGallerySaverPlugin, messenger: BinaryMessenger) {
+      val channel = MethodChannel(messenger, "image_gallery_saver")
+      channel.setMethodCallHandler(plugin)
     }
   }
+
+
 
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
     when {
@@ -33,7 +45,7 @@ class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandl
           val quality = call.argument<Int>("quality") ?: return
           val name = call.argument<String>("name")
 
-          result.success(saveImageToGallery(BitmapFactory.decodeByteArray(image,0,image.size), quality, name))
+          result.success(saveImageToGallery(BitmapFactory.decodeByteArray(image, 0, image.size), quality, name))
         }
         call.method == "saveFileToGallery" -> {
           val path = call.arguments as String
@@ -58,7 +70,7 @@ class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandl
   }
 
   private fun saveImageToGallery(bmp: Bitmap, quality: Int, name: String?): String {
-    val context = registrar.activeContext().applicationContext
+
     val file = generateFile("jpg", name = name)
     try {
       val fos = FileOutputStream(file)
@@ -67,7 +79,7 @@ class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandl
       fos.flush()
       fos.close()
       val uri = Uri.fromFile(file)
-      context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
+      context?.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
       bmp.recycle()
       return uri.toString()
     } catch (e: IOException) {
@@ -77,14 +89,14 @@ class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandl
   }
 
   private fun saveFileToGallery(filePath: String): String {
-    val context = registrar.activeContext().applicationContext
+
     return try {
       val originalFile = File(filePath)
       val file = generateFile(originalFile.extension)
       originalFile.copyTo(file)
 
       val uri = Uri.fromFile(file)
-      context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
+      context?.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
       return uri.toString()
     } catch (e: IOException) {
       e.printStackTrace()
@@ -93,20 +105,35 @@ class ImageGallerySaverPlugin(private val registrar: Registrar): MethodCallHandl
   }
 
   private fun getApplicationName(): String {
-    val context = registrar.activeContext().applicationContext
+
     var ai: ApplicationInfo? = null
-    try {
-        ai = context.packageManager.getApplicationInfo(context.packageName, 0)
-    } catch (e: PackageManager.NameNotFoundException) {
+    context?.let {
+      try {
+        ai = it.packageManager.getApplicationInfo(it.packageName, 0)
+      } catch (e: PackageManager.NameNotFoundException) {
+      }
     }
-    var appName: String
-    appName = if (ai != null) {
-      val charSequence = context.packageManager.getApplicationLabel(ai)
-      StringBuilder(charSequence.length).append(charSequence).toString()
-    } else {
-      "image_gallery_saver"
+
+    var appName: String = ""
+
+    context?.let {
+      appName = if (ai != null) {
+        val charSequence = it.packageManager.getApplicationLabel(ai)
+        StringBuilder(charSequence.length).append(charSequence).toString()
+      } else {
+        "image_gallery_saver"
+      }
     }
     return  appName
+  }
+
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    context = binding.applicationContext
+    register(this, binding.getBinaryMessenger());
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+
   }
 
 
